6e54f166857e923f930a87e643b0c5678bba0420
pendragon8 <ada0912281@gmail.com>
2019-06-29 10:20:36 +0800

    Add project files.

---
+++ DependencyInjectionWorkshop/Models/AuthenticationService.cs
@@ -0,0 +1,61 @@
+﻿using Dapper;
+using SlackAPI;
+using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+
+namespace DependencyInjectionWorkshop.Models
+{
+    public class AuthenticationService
+    {
+        public bool Verify(string accountId, string password, string otp)
+        {
+            string currentPassword;
+            using (var connection = new SqlConnection("datasource=db,password=abc"))
+            {
+                currentPassword = connection.Query<string>("spGetUserPassword", new { Id = accountId },
+                    commandType: CommandType.StoredProcedure).SingleOrDefault();
+            }
+
+            var crypt = new System.Security.Cryptography.SHA256Managed();
+            var hash = new StringBuilder();
+            var crypto = crypt.ComputeHash(Encoding.UTF8.GetBytes(password));
+            foreach (var theByte in crypto)
+            {
+                hash.Append(theByte.ToString("x2"));
+            }
+
+            var hashPassword = hash.ToString();
+
+            var httpClient = new HttpClient() { BaseAddress = new Uri("http://joey.com/") };
+            var response = httpClient.PostAsJsonAsync("api/otps", accountId).Result;
+            string currentOtp;
+            if (response.IsSuccessStatusCode)
+            {
+                currentOtp = response.Content.ReadAsAsync<string>().Result;
+            }
+            else
+            {
+                throw new Exception($"web api error, accountId:{accountId}");
+            }
+
+
+            if (hashPassword == currentPassword && otp == currentOtp)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public void Notify(string message)
+        {
+            var slackClient = new SlackClient("my api token");
+            slackClient.PostMessage(response => { }, "my channel", "my message", "my bot name");
+        }
+    }
+}
